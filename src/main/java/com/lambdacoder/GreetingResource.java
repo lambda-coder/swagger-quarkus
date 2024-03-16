8b24e583add549105907bad742b2103aa59b472c
Petr Pravda <petr.pravda@gmail.com>
2024-03-16 18:38:27 +0100

    resource annotated for swagger

--- src/main/java/com/lambdacoder/GreetingResource.java
+++ src/main/java/com/lambdacoder/GreetingResource.java
@@ -4,12 +4,17 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/hello")
+@Tag(name = "Example Greeting resource", description = "This is an example GreetingResource")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.TEXT_PLAIN))
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
